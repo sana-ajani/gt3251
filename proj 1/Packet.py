f43691d4dd9d7deeceabc5fd050cbee93b4ec2f7
suwilliam <williamsu@gatech.edu>
2016-11-14 17:58:19 -0500

    finished packet class

---
+++ proj 1/Packet.py
@@ -0,0 +1,12 @@
+class Packet:
+	def __init__(self, src_port, dest_port, seq_num, ack_num, flags, data, checksum = None, offset = None):
+		self.src_port = src_port & 0xffff
+		self.dest_port = dest_port & 0xffff
+		self.seq_num = seq_num
+		self.ack_num = ack_num
+		self.data = data
+		self.ACK = flags[0]
+		self.PSH = flags[1]
+		self.RST = flags[2]
+		self.SYN = flags[3]
+		self.FIN = flags[4]
