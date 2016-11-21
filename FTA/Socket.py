f9ef48152a1ac2896b4f6a9005703833c8668286
Sana Ajani <sanaajani@msn.com>
2016-11-21 13:40:15 -0500

    handshake complete

--- FTA/Socket.py
+++ FTA/Socket.py
@@ -5,6 +5,8 @@ import os
 import hashlib
 import logging
 import Packet
+from timeout import timeout
+#http://stackoverflow.com/questions/2281850/timeout-function-if-it-takes-too-long-to-finish
 
 #create socket objects and bind to ports, send and receive data, and create connections.
 class Socket:
@@ -32,7 +34,7 @@ class Socket:
     def create_packet(src_portNum, dest_portNum, seq_num, ack_num, flags, data, checksum = None):  #delete offset
         return Packet(src_portNum, dest_portNum, seq_num, ack_num, flags, data, checksum)
 
-
+    @timeout(5)
     #create and send SYN packet
     def send_SYN(self):
         #send SYN packet with no data and SYN flag on, sequence number = 0
@@ -50,6 +52,7 @@ class Socket:
         except self.socket.timeout:
             logging.debug("Send SYN timeout")
 
+    @timeout(5)
     #create and send SYN ACK packet
     def send_SYNACK(self):
         synack_pkt = create_packet(self.src_address[2], self.dest_address[2], self.seq_num, self.ack_num, [True, False, False, True, False], None)
@@ -57,10 +60,15 @@ class Socket:
         self.socket.sendto(synack_pkt, self.dest_address)
 
         try:
+            #while synack's timer is running
             ack, server = self.socket.recvfrom(self.window)
         except self.socket.timeout:
+            #send_SYNACK
             logging.debug("Send SYN ACK timeout")
+            #return none to client
+            #resend limit
 
+    @timeout(5)
     #create and send final ACK packet
     def send_ACK(self):
         ack_pkt = create_packet(self.src_address[2], self.dest_address[2], self.seq_num, self.ack_num, [True, False, False, False, False], None)
@@ -70,3 +78,22 @@ class Socket:
             data, server = self.socket.recvfrom(self.window)
         except self.socket.timeout:
             logging.debug("Send ACK timeout")
+
+
+    def wait_for_connect(self):
+
+        synack = self.send_SYN()
+
+        self.send_ACK()
+
+    #send data
+    def send(self, data):
+
+        #append data into queue in chunks
+        dataQueue = queue()
+
+        for i in range(0, len(data), 32):
+            if (len(data) < 32):
+                dataQueue.append(data[i:i+len(data)])
+            else:
+                dataQueue.append(data[i:])
